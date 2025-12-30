908b773f18b92bdcb6ccc8eff016229da448759f
Eric Green <eric_green86@hotmail.com>
2025-12-30 17:03:32 -0500

    Added resources for users.

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateSingleFieldDto } from '../dto/update-single-field.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  async findAll() {
+    return this.userModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    return this.userModel.findOne({ id }).exec();
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findOneAndUpdate({ id }, updateUserDto, { new: true }).exec();
+  }
+
+  async updateField(updateSingleFieldDto: UpdateSingleFieldDto) {
+    const { id, fieldName, value } = updateSingleFieldDto;
+    return this.userModel.findOneAndUpdate(
+      { id },
+      { [fieldName]: value },
+      { new: true }
+    ).exec();
+  }
+
+  async remove(id: string) {
+    return this.userModel.findOneAndDelete({ id }).exec();
+  }
+}
