6ca17b500c1041080a69d9b4dd03dd475de0a593
Eric Green <eric_green86@hotmail.com>
2025-12-30 10:44:19 -0500

    Formatting changes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthCategoriesModule } from './health-categories/health-categories.module';
 
 @Module({
-  imports: [],
+  imports: [HealthCategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
