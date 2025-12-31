d21ae3c33d3111bd5cf50d78d9fd37415f4aacb2
Eric Green <eric_green86@hotmail.com>
2025-12-31 09:28:37 -0500

    Formatting changes.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -6,6 +6,7 @@ describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
