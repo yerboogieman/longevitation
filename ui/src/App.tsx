17651e185833f99e14e6dd131230f190a0049a58
Eric Green <eric_green86@hotmail.com>
2026-01-01 22:48:39 -0500

    Created a new front end app in the./ui directory.

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@customation/ui-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Container />
+        </div>
+    );
+}
+
+export default App;
