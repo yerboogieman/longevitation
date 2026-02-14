ada51a89774d1f17c7e11abcb7a576151ffdcf3b
Eric Green <eric_green86@hotmail.com>
2026-02-14 00:04:11 -0500

    New diet sub-pages.

---
+++ ui/src/components/diet/Recipes.tsx
@@ -0,0 +1,10 @@
+function Recipes() {
+    return (
+        <div style={{padding: "24px"}}>
+            <h6>Recipes</h6>
+            <p style={{color: "#6c757d"}}>Your saved recipes will appear here.</p>
+        </div>
+    );
+}
+
+export default Recipes;
