17a16d0a8bde9c7bf5ecac485bc65d8a576c47b6
Eric Green <eric_green86@hotmail.com>
2026-02-14 00:08:55 -0500

    Added ideas to lifestyle tracker.

--- ui/src/components/lifestyle/Tracker.tsx
+++ ui/src/components/lifestyle/Tracker.tsx
@@ -6,6 +6,7 @@ function Tracker() {
                 <ul>
                     <li>Notifications to pull up daily events wizard/questionnaire.</li>
                     <li>Track with app: "Hey siri health track a Starbucks grande decaf with 4 tablespoons of heavy cream"</li>
+                    <li>Offer health coaching service. Users can pay for monthly or a one-time fee for some quick help.</li>
                 </ul>
             </p>
         </div>
